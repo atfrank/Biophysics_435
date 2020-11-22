6edc3bbb472bd202220b96061f1412aaa249a9f2
Aaron T Frank <afrankz@umich.edu>
2020-11-22 11:52:58 -0500

    updated protein example

---
+++ 2020/.virtual_documents/8. B. Final Assignment 3D Ensemble (Protein Alternate).ipynb.py
@@ -0,0 +1,16 @@
+get_ipython().run_cell_magic("capture", "", """!git clone https://github.com/sbottaro/BME.git
+import BME.bme_reweight as bme""")
+
+
+get_ipython().run_cell_magic("capture", "", """import pandas as pd
+import numpy as np
+from reweighting import *""")
+
+
+# add code here
+
+
+# add code here
+
+
+# add code here
